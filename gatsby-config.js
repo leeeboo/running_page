541737a166a0c680249f62d4c421913e21aafd30
Albert Lee <leeeboo@users.noreply.github.com>
2023-06-16 06:29:05 +0000

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,18 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: '黑影儿跑步的日子',
+    siteUrl: 'https://runningpage.libo.run',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: '致那些抗争的日子',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
+        name: 'YouTube',
+        url: 'https://www.youtube.com/channel/UCBvcXPwXIRhKuTxe-6sVT8A',
       },
       {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: '哔哩哔哩',
+        url: 'https://space.bilibili.com/38995440',
       },
     ],
   },
