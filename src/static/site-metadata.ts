e61d3769260d577feadb0d7e4f8b366c2324aef5
Albert Lee <leeeboo@users.noreply.github.com>
2024-08-23 10:28:48 +0900

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,26 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: '黑影儿的跑步记录',
+  siteUrl: 'https://runningpage.libo.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: '跑过的路，都不会白费',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@shadow-runner',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'BiliBili',
+      url: 'https://space.bilibili.com/38995440',
+    },
+    {
+      name: 'WeChat',
+      url: 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MjM5MTUxMDgwNA==&scene=124#wechat_redirect',
+    },
+    {
+      name: 'Patreon',
+      url: 'https://www.patreon.com/shadowrunning',
     },
   ],
 };
